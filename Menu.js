9293b16efa48051b3fb4336cafbe17ed8f11a160
Cheris <lamtamnhu2017cm@gmail.com>
2023-07-28 02:56:13 +0700

    first commit

--- Menu.js
+++ Menu.js
@@ -8,6 +8,19 @@ items.forEach(li => {
         })
         this.classList.add('active');
     })
-
 })
+
+document.getElementById('img-cream').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+/*document.getElementById('img-cream2').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[2]);
+}*/
+
+/*document.getElementById('active').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.Length - 1]);
+}*/
 
